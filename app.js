9283dd35e4e082b654f7ac1b2be76de79effc773
mohammad albasha <hamoudy00128@hotmail.com>
2022-05-19 16:01:40 +0200

    fix bug

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ db.connect()
 
 // catchings errors
 app.use((error, req, res, next) => {
-  console.log(error);
   const status = error.statusCode || 500;
-  const message = error;
-  res.status(status).json({ message: message});
+  const message = error.message;
+  const date = error.data;
+  res.status(status).json({status, message, data});
 });
