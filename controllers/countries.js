da88a190ba405dff3069febba248e24c5ffa745e
mohammad albasha <hamoudy00128@hotmail.com>
2022-05-21 00:27:34 +0200

    add general informations about countries

--- controllers/countries.js
+++ controllers/countries.js
@@ -8,19 +8,50 @@ function getGeneralInformations (category, value){
             url: `https://restcountries.com/v3.1/${category}/${value}`
         })
         .then (respnose => {
-            console.log(respnose.data[0].name.official);
-            return respnose.data;
+            return extractInformations(respnose.data[0]);
         })
     );  
         
 }
 
+function extractInformations (informations){
+    const name  = {
+        common : informations.name.common,
+        official : informations.name.official,
+        nativeName : informations.name.nativeName[Object.keys(informations.name.nativeName)[0]].official
+    }
+    const currency = {
+        code : Object.keys(informations.currencies)[0],
+        name : informations.currencies[Object.keys(informations.currencies)[0]].name,
+        symbol : informations.currencies[Object.keys(informations.currencies)[0]].symbol
+    };
+    const { idd, maps, population, car, startOfWeek, timezones, languages } = informations
+    const flag = informations.flags.png;
+    const capital = informations.capital[0];
+    const location = {
+        countryLocation : `https://www.google.com/maps/search/?api=1&query=${informations.latlng[0]},${informations.latlng[1]}`,
+        capitalLocation : `https://www.google.com/maps/search/?api=1&query=${informations.capitalInfo.latlng[0]},${informations.capitalInfo.latlng[1]}`
+    }
+    return {
+        name,
+        currency,
+        capital,
+        languages,
+        location,
+        maps,
+        timezones,
+        flag,
+        population,
+        car,
+        startOfWeek,
+        idd
+    }
+}
 
 module.exports.getGeneralInformations = (req,res,next) => {
     const category = req.params.category;
     const value = req.params.value;
 
-    
     getGeneralInformations(category,value)
     .then (informations => {
         res.status(200).json(informations);
